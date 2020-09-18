8c598086150abda60c2ed088c32175be527bbb6c
erxian <evelynhsu21@gmail.com>
2020-09-18 16:56:29 +0800

    add split_bill.py for problem1

---
+++ Labs/lab1/split_bill.py
@@ -0,0 +1,20 @@
+'''
+Zengping Xu
+CS 5001, Fall 2020
+
+This program gets input from the users about the value of a resterant bill and calculate 
+how much each person should contribute. 
+'''
+
+def main():
+    amount = float(input("how much was the bill? "))
+    percent = float(input("what percentage of tip would everyone prefer? (please enter 0-1): "))
+    guests = int(input("how many people are in the group? "))
+
+    total_fees = amount + amount*percent
+    split_amount = total_fees/guests
+    print("every group member should pay", split_amount)
+
+
+if __name__ == "__main__":
+    main()
